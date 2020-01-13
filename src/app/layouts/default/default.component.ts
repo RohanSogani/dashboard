b61a3b8a62059c2bc20b71c2e348da30cf5bc71b
RohanSogani <ronsogani@gmail.com>
2020-01-13 10:09:03 -0800

    Bug fix

--- src/app/layouts/default/default.component.ts
+++ src/app/layouts/default/default.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultComponent implements OnInit {
 
+  sideBarOpen = true;
   constructor() { }
 
   ngOnInit() {
   }
+
+  
 
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from 'protractor';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
   }
+
+  
 
 }
