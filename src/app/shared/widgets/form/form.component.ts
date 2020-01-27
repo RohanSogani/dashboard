ebf7346dd13ba2704c0f9746e7648ef6d970d4a4
RohanSogani <ronsogani@gmail.com>
2020-01-27 14:45:45 -0800

    Form changes

--- src/app/shared/widgets/form/form.component.ts
+++ src/app/shared/widgets/form/form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
+import { Validators } from '@angular/forms'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Component({
@@ -7,8 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  
+  runForm = new FormGroup({
+    numberOfNodes: new FormControl('', Validators.required),
+    numberOfClients: new FormControl('', Validators.required),
+    batchSize: new FormControl('', Validators.required),
+  })
+  URL = "http://localhost:2000/run";
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+  
 
   ngOnInit() {
   }
@@ -16,8 +28,21 @@ export class FormComponent implements OnInit {
     console.log(x); 
   }
 
-  runEvent(event) {
+  /* runEvent(event) {
     //call backend
     console.log("Run Clicked");
+  } */
+
+  onSubmit() {
+    console.log(this.runForm.value);
+    this.http.post(this.URL, this.runForm.value, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'my-auth-token'
+      })
+    }).subscribe();
+    console.log("yay");
   }
 }
+
+
